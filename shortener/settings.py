2b350ca545aa8519622a590c457fafb92f94ac2e
Vaibhav Arora <vaibhavarora0301vishu@gmail.com>
2023-03-11 19:15:38 +0530

    env variables

--- shortener/settings.py
+++ shortener/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x7&o-^%w*z#=tm#vkrr0nxbj33@c(5(yc_g@25p=q=ed3g641b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
